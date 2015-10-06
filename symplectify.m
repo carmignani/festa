d9465d22156dacbfbfe0af90e9972d4334be649c
ncarmignani <n.carmignani@gmail.com>
2015-10-06 15:26:54 +0200

    added the symplectify matlab function

---
+++ symplectify.m
@@ -0,0 +1,15 @@
+function MS=symplectify(M)
+%symplectify makes a matrix more symplectic
+%follow Healy algorithm as described by McKay (2006)
+%BNL-75461-2006-CP
+
+ 
+J=jmat(3);
+V=J*(eye(6)-M)*inv(eye(6)+M);
+%V should be almost symmetric.  Replace with symmetrized version.
+
+ 
+W=(V+V')/2;
+%Now reconstruct M from VS
+MS=(eye(6)+J*W)*inv(eye(6)-J*W);
+
